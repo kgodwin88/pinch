ea4c3a74518a9ecf1ec1d3b16400e112ded03fbe
kgodwin88 <kgodwin1988@aol.com>
2018-06-26 20:30:32 -0400

    changes

--- models/potty.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(sequelize, DataTypes){
-    var Potty = sequelize.define("Potty", function(){
-        //placeholder variable
-        location: DataTypes.STRING
-    });
-    return Potty;
-}
